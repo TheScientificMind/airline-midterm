1d459f826b9eeeedd30684eb8d38ca0921a7b760
Colin <credmond@apcsp.io>
2023-01-13 12:39:45 -0500

    added two classes

--- Ticket_Purchasing_Interface.py
+++ Ticket_Purchasing_Interface.py
@@ -2,5 +2,19 @@ import sqlib
 
 con= sqlib.create_db_connection ("127.0.0.1", "credmond", "credmond1", "flights")
 
-class Ticket:
+
+
+class booking_entry:
+    Name = None
+    Phone_number = None
+    Email = None
+    Confirmation_Code = None
+    class Ticket:
+        Airport_Code = None
+        Date = None
+        Price = None
+        Time =  None
+    
+
+
     
