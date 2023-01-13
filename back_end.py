adfd910b27c8e952029c7604da06c9a97a2d8128
Annie <ablischer@icloud.com>
2023-01-13 13:02:46 -0500

    stuff was done YAY

--- back_end.py
+++ back_end.py
@@ -1,16 +1,35 @@
 import sqlib
+from_db = []
 con = sqlib.create_db_connection("127.0.0.1","alischer","alischer1","airline")
 
-ticket = 0
+uinput = list(map(int,input("Input Flight ID: ").strip().split()))
+
+qt = """select flights.id,capacity.aircraft from flights 
+join capacity on flights.aircraft = capacity.aircraft;""".format(id=uinput[0])
+
+query_results = sqlib.read_query(con,qt)
+for x in query_results:
+        x = list(x)
+        from_db.append(x)
+
+uinput = uinput[0] - 1
+
+print(from_db[uinput][1])
 
 class flier:
     fname = None
     lname = None
     phone_num = None
     email = None
+    ffmiles = None
 
     def __init__(self,fname,lname,phone_num,email):
-        self.fname =
-        self.lname = 
-        self.phone_num = 
-        self.email = 
+        self.fname = fname
+        self.lname = lname
+        self.phone_num = phone_num
+        self.email = email
+        # all based upon email
+        # self.ffmiles = 
+    
+    def addff(self,ffmiles):
+        self.ffmiles =+ ffmiles
