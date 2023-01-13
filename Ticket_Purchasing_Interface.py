32837a16b06f3345fe23fd3998e1f3a4336ab02c
Colin <credmond@apcsp.io>
2023-01-13 13:01:42 -0500

    added code

--- Ticket_Purchasing_Interface.py
+++ Ticket_Purchasing_Interface.py
@@ -2,19 +2,33 @@ import sqlib
 
 con= sqlib.create_db_connection ("127.0.0.1", "credmond", "credmond1", "flights")
 
+def Buy_ticket (Z):
+    class booking_entry:
+         fName = None
+         lname = None
+         Phone_number = None
+         Email = None
+   
+         def __init__ (self, fname, lname, Phone_Number, Email):
+            self.fname = fname
+            self.lname = lname
+            self.Phone_Number = Phone_Number
+            self.Email = Email
 
+         class Ticket:
+            Airport_Code = None
+            Date = None
+            Price = None
+            Time =  None
+            Confirmation_Code = None
+            def __init__(self, Airport_Code, Date, Price, Time, Confirmation_Code):
+                self.Airport_Code = Airport_Code
+                self.Date = Date
+                self.Price = Price
+                self.Time = Time
+                self.Confirmation_Code = Confirmation_Code
+Buy_ticket(input(int("How many tickets do you want to buy:")))
 
-class booking_entry:
-    Name = None
-    Phone_number = None
-    Email = None
-    Confirmation_Code = None
-    class Ticket:
-        Airport_Code = None
-        Date = None
-        Price = None
-        Time =  None
-    
 
 
     
