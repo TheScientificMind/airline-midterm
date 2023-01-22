af86a4e47c9ca7f96c22568bd2b1a7da163f562c
Colin <credmond@apcsp.io>
2023-01-22 16:02:41 -0500

    added insert db functions,m needs more work

--- Ticket_Purchasing_Interface.py
+++ Ticket_Purchasing_Interface.py
@@ -13,22 +13,24 @@ class booking_entry:
          self.fname = fname
          self.lname = lname
          self.Phone_Number = Phone_Number
-         self.Email = Email
+         self.Email = Email 
 class Ticket:
+   flightid=  None
    Departure_Airport = None
    Arrival_Airport = None
    Datetime = None
    Price = None
    Aircraft = None
    Confirmation_Code = None
-   def __init__(self, Airport_Code1,Airport_Code2, Datetime, Price, Aircraft, Confirmation_Code):
+   def __init__(self, FlightID, Airport_Code1,Airport_Code2, Datetime, Price, Aircraft, Confirmation_Code):
+      self.flightid = FlightID
       self.Departure_Airport = Airport_Code1
       self.Arrival_Airport = Airport_Code2
       self.Datetime = Datetime
       self.Price = Price
       self.Aircraft= Aircraft
       self.Confirmation_Code = Confirmation_Code
-def Buy_Ticket (X):
+def Buy_Ticket (tickets_purchased):
    bookingentry = booking_entry(input("First name:"),input("Last name:"), input("Phone number:"),input("Email:"))
    print (bookingentry.fname, bookingentry.lname, bookingentry.Phone_Number, bookingentry.Email)
    y = input("Is this the correct booking entry:")
@@ -43,18 +45,26 @@ def Buy_Ticket (X):
          for x in flight:
             x = list(x)
             theflight.append(x)
-         def Create_Ticket (x, y, c, a, f, g):
-            ticket = Ticket(x, y, c, "$"+ a ,f, g )
-            print(ticket.Departure_Airport,ticket.Arrival_Airport, ticket.Datetime, ticket.Price, ticket.Aircraft, ticket.Confirmation_Code)
-         for u in range(int(X)):
+         def Create_Ticket (j,x, y, c, a, f, g):
+            ticket = Ticket(j,x, y, c, "$"+ a ,f, g )
+            print(ticket.flightid,ticket.Departure_Airport,ticket.Arrival_Airport, ticket.Datetime, ticket.Price, ticket.Aircraft, ticket.Confirmation_Code)
+            cquery = """INSERT INTO Bookings (booking_id, fname, lname , email, phone, flight_id,booking_price, purchase_date, ticket_amount, return_flights, confirmation_code, aircraft_type) 
+            VALUES ({bID},{fnam},{lnam},{theemail},{phonen},{idflight},{pricen},{thedated},{amountn},{returned},{coded},{typeofplane}) 
+            """.format(bID=A, fnam=bookingentry.fname, lnam=bookingentry.lname,theemail=bookingentry.Email, phonen=bookingentry.Phone_Number,idflight=ticket.flightid,
+            pricen=ticket.Price,thedated=ticket.Datetime, amountn=tickets_purchased, returned=Z, coded=ticket.Confirmation_Code, typeofplane=ticket.Aircraft )
+            g= sqlib.execute_query(con,cquery)
+         for u in range(int(tickets_purchased)):
             R = str(random.randint(10000000, 99999999))
-            Create_Ticket(str(theflight [0][1]), str(theflight [0][2]), str(theflight [0][3]), str(theflight [0][4]), str(theflight [0][5]), R)
+            Create_Ticket(str(theflight[0][0]),str(theflight [0][1]), str(theflight [0][2]), str(theflight [0][3]), str(theflight [0][4]), str(theflight [0][5]), R)
+         #iquery = "INSERT INTO flights (seats_booked) VALUES ('{x}') ".format(x=tickets_purchased)
+         #c = sqlib.execute_query(con,iquery)
+        
    if y == "yes": 
       thing_a_majig(input("please enter origin airport:"), input("please enter destination airport:"))
    elif y =="no":
       bookingentry = booking_entry(input("First name:"),input("Last name:"), input("Phone number:"),input("Email:"))
       thing_a_majig(input("please enter origin airport:"), input("please enter destination airport:"))
-   rquery = """ INSERT INTO flights WHERE """
+   #rquery = """ INSERT INTO flights WHERE """
 Buy_Ticket(input("How many tickets do you want to buy:"))
 
    
