5d6de8e4ced05490f25494c5024b4af9523a66c9
Annie <ablischer@icloud.com>
2023-01-24 12:14:50 -0500

    turned into functions and some small fixes.

--- back_end.py
+++ back_end.py
@@ -3,71 +3,74 @@ import math
 from_db = []
 con = sqlib.create_db_connection("127.0.0.1","alischer","alischer1","airline")
 
-uinput = int(input("Input Booking ID: "))
+uinput = int(input("Input FLight ID: "))
 
-qt = """select * from bookings where booking_id='{uinput}';""".format(uinput = uinput)
-query_results = sqlib.read_query(con,qt)
-for x in query_results:
+def ticket_checking(uinput):
+    qt = """select * from flights where id='{uinput}';""".format(uinput = uinput)
+    query_results = sqlib.read_query(con,qt)
+    for x in query_results:
         x = list(x)
         from_db.append(x)
-flight_id = from_db[0][5]
-aircraft = from_db[0][-1]
+    flight_id = from_db[0][0]
+    aircraft = from_db[0][5]
 
-tt = """select id,seats_booked from flights where id='{id}';""".format(id = flight_id)
+    tt = """select id,seats_booked from flights where id='{id}';""".format(id = flight_id)
 
-ticket_checker = sqlib.read_query(con,tt)
-tt_db = []
-for y in ticket_checker:
-    y = list(y)
-    tt_db.append(y)
-tickets = tt_db[0][1]
+    ticket_checker = sqlib.read_query(con,tt)
+    tt_db = []
+    for y in ticket_checker:
+        y = list(y)
+        tt_db.append(y)
+    tickets = tt_db[0][1]
 
-if aircraft == "737":
-    seats = 25
-elif aircraft == "747":
-    seats = 100
-elif aircraft == "767":
-    seats = 50
-elif aircraft == "777":
-    seats = 75
-elif aircraft == "A320":
-    seats = 25
-else:
-    print("error")
-
-if tickets > seats:
-    if input("I'm sorry but your requested flight is full. Press 1 if you would like to book another flight. Press 2 if you would like to end.") == "1":
-        print("do booking stuff")
+    if aircraft == "737":
+        seats = 25
+    elif aircraft == "747":
+        seats = 100
+    elif aircraft == "767":
+        seats = 50
+    elif aircraft == "777":
+        seats = 75
+    elif aircraft == "A320":
+        seats = 25
     else:
-        print("goodbye")
-else:
-    print("Your ticket has been booked.")
+        print("error")
 
-email = "placeholder@test.edu"
-try:
-    query = """select * from ffaccounts where email ='{email}';""".format(email = email)
-    priceq = """select booking_price from bookings where email ='{email}';""".format(email = email)
-    price_checker = sqlib.read_query(con,priceq)
-    price = price_checker[0][0]
-    ff = round(price/10)
-    ffaccount = sqlib.read_query(con,query)
-    new_ff = ff + ffaccount[0][2]
-    add_ff = """update ffaccounts set ffmiles =('{new_ff}') where ffmiles =('{ffaccount}') and email =('{email}');""".format(new_ff = new_ff, ffaccount = ffaccount[0][2], email = email)
-    sqlib.execute_query(con,add_ff)
-except:
-    if int(input("Would you like to create a frequent flier account? 1 for yes, 0 for no: ")) > 0:
-        password = str(input("Please enter a password for your frequent flier account: "))
-        # INSERT DOESN'T WORK ^ ask Dylan for assistance/explination
-        createrq = """insert into ffaccounts (email,password,ffmiles) values ('{email}','{password}','0');""".format(email = email, password = password)
-        sqlib.execute_query(con,createrq)
+    if tickets > seats:
+        if input("I'm sorry but your requested flight is full. Press 1 if you would like to book another flight. Press 2 if you would like to end.") == "1":
+            print("do booking stuff")
+        else:
+            print("goodbye")
+    else:
+        print("Your ticket has been booked.")
+
+def ffmiles(email):
+    try:
+        query = """select * from ffaccounts where email ='{email}';""".format(email = email)
         priceq = """select booking_price from bookings where email ='{email}';""".format(email = email)
         price_checker = sqlib.read_query(con,priceq)
         price = price_checker[0][0]
         ff = round(price/10)
-        new_ff = ff
-        add_ff = """update ffaccounts set ffmiles =('{new_ff}') where ffmiles =('0') and email =('{email}');""".format(new_ff = new_ff, ffaccount = 0, email = email)
-    else:
-        print("Understood")
+        ffaccount = sqlib.read_query(con,query)
+        new_ff = ff + ffaccount[0][2]
+        print(f"You have gained {ff} frequent flier miles for a new total of {new_ff} miles.")
+        add_ff = """update ffaccounts set ffmiles =('{new_ff}') where ffmiles =('{ffaccount}') and email =('{email}');""".format(new_ff = new_ff, ffaccount = ffaccount[0][2], email = email)
+        sqlib.execute_query(con,add_ff)
+    except:
+        if int(input("Would you like to create a frequent flier account? 1 for yes, 0 for no: ")) > 0:
+            password = str(input("Please enter a password for your frequent flier account: "))
+            createrq = """insert into ffaccounts (email,password,ffmiles) values ('{email}','{password}','0');""".format(email = email, password = password)
+            sqlib.execute_query(con,createrq)
+            priceq = """select booking_price from bookings where email ='{email}';""".format(email = email)
+            price_checker = sqlib.read_query(con,priceq)
+            price = price_checker[0][0]
+            ff = round(price/10)
+            new_ff = ff
+            print(f"You now have {new_ff} frequent flier miles!")
+            add_ff = """update ffaccounts set ffmiles =('{new_ff}') where ffmiles =('0') and email =('{email}');""".format(new_ff = new_ff, ffaccount = 0, email = email)
+            sqlib.execute_query(con,add_ff)
+        else:
+            print("Understood")
 
 ## 22000 Total
 # 679 DEL origins
